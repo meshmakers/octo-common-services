e257e0572779e291e9c95b76e492101ad3f80d5c
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-01-17 23:39:14 +0100

    Ensure that the same tenant is not handled multiple times in parallel.

--- src/Infrastructure/Consumers/PosCreateTenantConsumer.cs
+++ src/Infrastructure/Consumers/PosCreateTenantConsumer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Meshmakers.Octo.Common.DistributionEventHub.Consumers;
 using Meshmakers.Octo.Services.Common.DistributionEventHub.Messages;
 using Meshmakers.Octo.Services.Infrastructure.Services;
@@ -7,14 +8,27 @@ namespace Meshmakers.Octo.Services.Infrastructure.Consumers;
 internal class PosCreateTenantConsumer: IDistributedConsumer<PosCreateTenant>
 {
     private readonly IDefaultConfigurationCreatorService _defaultConfigurationCreatorService;
+    private readonly ConcurrentDictionary<string, bool> _tenantsInHandling = new();
 
     public PosCreateTenantConsumer(IDefaultConfigurationCreatorService defaultConfigurationCreatorService)
     {
         _defaultConfigurationCreatorService = defaultConfigurationCreatorService;
     }
 
-    public Task ConsumeAsync(IDistributedContext<PosCreateTenant> context)
+    public async Task ConsumeAsync(IDistributedContext<PosCreateTenant> context)
     {
-        return _defaultConfigurationCreatorService.SetupAsync(context.Message.TenantId);
+        if (!_tenantsInHandling.TryAdd(context.Message.TenantId, true))
+        {
+            return;
+        }
+
+        try
+        {
+            await _defaultConfigurationCreatorService.SetupAsync(context.Message.TenantId);
+        }
+        finally
+        {
+            _tenantsInHandling.Remove(context.Message.TenantId, out _);
+        }
     }
 }
