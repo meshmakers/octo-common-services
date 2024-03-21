343b355bc070cda90250f6db4d6ca33420b8382e
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-03-21 18:31:20 +0100

    Table columns under "" to preserve capitalization

--- src/Timeseries/Queries.cs
+++ src/Timeseries/Queries.cs
@@ -5,8 +5,8 @@ internal static class Queries
 {
     public const string CreateTableIfNotExists = """
                                                  create table if not exists {0}(
-                                                 CkTypeId TEXT not null,
-                                                 RtId TEXT not null,
+                                                 "CkTypeId" TEXT not null,
+                                                 "RtId" TEXT not null,
                                                  "Timestamp" TIMESTAMP WITH TIME ZONE not null,
                                                  data object(Dynamic)
                                                  ) clustered into 3 shards;
@@ -15,5 +15,5 @@ internal static class Queries
     public const string DeleteTableIfExists = "drop table if exists {0};";
 
     public const string InsertTimeSeriesEntry =
-        """insert into {0} (RtId, CkTypeId, "Timestamp", data) values (@RtId, @CkTypeId, @Timestamp, @data);""";
+        """insert into {0} ("RtId", "CkTypeId", "Timestamp", data) values (@RtId, @CkTypeId, @Timestamp, @data);""";
 }
