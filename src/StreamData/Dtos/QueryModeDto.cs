5c0afcf381e19fe27918b256f8aa3da10a4ed3ed
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-05-07 11:21:47 +0200

    Include a Query Mode Dto; Rename From TimeFilter to stream data arguments

---
+++ src/StreamData/Dtos/QueryModeDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Meshmakers.Octo.Services.Common.StreamData.Dtos;
+
+/// <summary>
+/// Defines the kind of query to be executed
+/// </summary>
+public enum QueryModeDto
+{
+    /// <summary>
+    /// Returns the real values
+    /// </summary>
+    Default,
+    
+    /// <summary>
+    /// Aggregates the values
+    /// </summary>
+    Aggregation,
+    
+    /// <summary>
+    /// Interpolates the values
+    /// </summary>
+    Interpolation
+}
