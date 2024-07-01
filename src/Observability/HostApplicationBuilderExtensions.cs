91c6b56f4c5a45c9e9b94259fb29e2d95e7bf822
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-07-01 15:34:54 +0200

    AB#1366 Add extension method to be able to add health checks to our mesh adapter.

---
+++ src/Observability/HostApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Meshmakers.Octo.Services.Observability;
+
+public static class HostApplicationBuilderExtensions
+{
+    /// <summary>
+    /// Add observability to the host application
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    public static IHealthChecksBuilder AddObservability(this IHostApplicationBuilder builder)
+    {
+        return new ObservabilityBuilder(builder.Configuration, builder.Services, builder.Environment)
+            .AddObservability();
+    }
+}
