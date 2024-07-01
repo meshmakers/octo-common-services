72366f22e3c082e2c67fb771a94d9ad9da46d7a5
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-07-01 13:18:50 +0200

    AB#1366 Add observability to our services

---
+++ src/Observability/WebApplicationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Observability;
+
+/// <summary>
+/// Extensions for the <see cref="WebApplication"/> class.
+/// </summary>
+public static class WebApplicationExtensions
+{
+    /// <summary>
+    /// Map the endpoints for observability
+    /// </summary>
+    /// <param name="app"></param>
+    public static void MapObservability(this WebApplication app)
+    {
+        app.MapPrometheusScrapingEndpoint();
+        app.MapHealthChecks("/health");
+    }
+}
