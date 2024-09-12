05e42ce8b94fcb1090055a696a8904168ac1ab4f
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-09-12 16:28:22 +0200

    #AB1463: Fix: result object may be optional

--- src/Common/DistributionEventHub/Commands/ExecuteMeshPipelineResponse.cs
+++ src/Common/DistributionEventHub/Commands/ExecuteMeshPipelineResponse.cs
@@ -3,4 +3,4 @@ namespace Meshmakers.Octo.Services.Common.DistributionEventHub.Commands;
 /// <summary>
 ///     Response after executing a mesh pipeline
 /// </summary>
-public record ExecuteMeshPipelineResponse(bool IsSuccess, string? ErrorMessage, string PipelineOutput);
+public record ExecuteMeshPipelineResponse(bool IsSuccess, string? ErrorMessage, string? PipelineOutput);
