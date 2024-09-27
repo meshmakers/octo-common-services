4fd3014d43c1af935e23bef9cbc9905f85d8ba6a
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-09-27 11:31:06 +0200

    AB#1408 Implement Update of TS data; set primary key to RtId + Timestamp

--- src/StreamData/Queries.cs
+++ src/StreamData/Queries.cs
@@ -6,8 +6,8 @@ internal static class Queries
     public const string CreateTableIfNotExists = """
                                                  create table if not exists {0}(
                                                  "CkTypeId" TEXT not null,
-                                                 "RtId" TEXT not null,
-                                                 "Timestamp" TIMESTAMP WITH TIME ZONE not null,
+                                                 "RtId" TEXT not null primary key,
+                                                 "Timestamp" TIMESTAMP WITH TIME ZONE not null primary key,
                                                  data object(Dynamic)
                                                  ) clustered into 3 shards;
                                                  """;
@@ -15,8 +15,18 @@ internal static class Queries
     public const string DeleteTableIfExists = "drop table if exists {0};";
 
     public const string InsertStreamDataEntry =
-        """insert into {0} ("RtId", "CkTypeId", "Timestamp", data) values (@RtId, @CkTypeId, @Timestamp, @data);""";
+        """
+        insert into {0} ("RtId", "CkTypeId", "Timestamp", data)
+        values (@RtId, @CkTypeId, @Timestamp, @data)
+        ON CONFLICT ("Timestamp", "RtId")
+        DO UPDATE SET "data" = "data" || EXCLUDED."data";
+        """;
 
     public const string InsertStreamDataBulk =
-        """insert into {0}("RtId", "CkTypeId", "Timestamp", "data") SELECT * FROM unnest(@rtIds, @ckTypeIds, @timestamps, @data);""";
+        """
+        INSERT INTO {0} ("RtId", "CkTypeId", "Timestamp", "data")
+        SELECT * FROM unnest(@rtIds, @ckTypeIds, @timestamps, @data)
+        ON CONFLICT ("Timestamp", "RtId")
+        DO UPDATE SET "data" = "data" || EXCLUDED."data";
+        """;
 }
