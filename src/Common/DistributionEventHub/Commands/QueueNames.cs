3330b551dc7c3d8d073275c02f2e83de7d8bded0
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-09-29 11:51:27 +0200

    AB#1476: New: synchronized names of queues/exchanges

--- src/Common/DistributionEventHub/Commands/QueueNames.cs
+++ src/Common/DistributionEventHub/Commands/QueueNames.cs
@@ -8,37 +8,37 @@ public static class QueueNames
     /// <summary>
     /// Import a construction kit library command
     /// </summary>
-    public const string ImportCkCommand = "bot::import-ck"; 
+    public const string ImportCkCommand = "octo::bot::import-ck"; 
     
     /// <summary>
     /// Import a runtime model command
     /// </summary>
-    public const string ImportRtCommand = "bot::import-rt";
+    public const string ImportRtCommand = "octo::bot::import-rt";
 
     /// <summary>
     /// Export a runtime model by query command
     /// </summary>
-    public const string ExportRtByQueryCommand = "bot::export-rt-by-query";
+    public const string ExportRtByQueryCommand = "octo::bot::export-rt-by-query";
     
     /// <summary>
     /// Export a runtime model by deep graph command
     /// </summary>
-    public const string ExportRtByDeepGraphCommand = "bot::export-rt-by-deep-graph";
+    public const string ExportRtByDeepGraphCommand = "octo::bot::export-rt-by-deep-graph";
     
     /// <summary>
     /// Remove recurring jobs by schedule group command
     /// </summary>
-    public const string RemoveRecurringJobsByScheduleGroupCommand = "bot::remove-recurring-jobs-by-schedule-group";
+    public const string RemoveRecurringJobsByScheduleGroupCommand = "octo::bot::remove-recurring-jobs-by-schedule-group";
     
     /// <summary>
     /// Create identity data command
     /// </summary>
-    public const string CreateIdentityDataCommand = "identity::create-identity-data";
+    public const string CreateIdentityDataCommand = "octo::identity::create-identity-data";
     
     /// <summary>
     /// The pipeline trigger channel name
     /// </summary>
-    public const string PipelineTriggerChannelName = "bot::pipeline-trigger";
+    public const string PipelineTriggerChannelName = "octo::bot::pipeline-trigger";
     
     /// <summary>
     /// The pipeline trigger queue
@@ -48,5 +48,5 @@ public static class QueueNames
     /// <summary>
     /// Execute mesh pipeline command
     /// </summary>
-    public const string ExecuteMeshPipelineCommand = "com-controller::execute-mesh-pipeline";
+    public const string ExecuteMeshPipelineCommand = "octo::com-controller::execute-mesh-pipeline";
 }
