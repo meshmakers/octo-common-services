9fa5825b9589b92b8fa7bb0e5ecf64c80570a6c0
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-10-01 14:10:28 +0200

    Add RtCreationDateTime, RtChangedDateTime and RtWellKnownName to the TS entries

--- src/StreamData/Queries.cs
+++ src/StreamData/Queries.cs
@@ -4,30 +4,39 @@ namespace Meshmakers.Octo.Services.Common.StreamData;
 internal static class Queries
 {
     public const string CreateTableIfNotExists = """
-                                                 create table if not exists {0}(
-                                                 "CkTypeId" TEXT not null,
-                                                 "RtId" TEXT not null,
-                                                 "Timestamp" TIMESTAMP WITH TIME ZONE not null,
-                                                 data object(Dynamic),
-                                                 PRIMARY KEY ("Timestamp", "RtId", "CkTypeId")
-                                                 ) clustered into 3 shards;
+                                                 CREATE TABLE IF NOT EXISTS {0} (
+                                                   "CkTypeId" TEXT NOT NULL,
+                                                   "RtId" TEXT NOT NULL,
+                                                   "Timestamp" TIMESTAMP WITH TIME ZONE NOT NULL,
+                                                   "RtCreationDateTime" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                                                   "RtChangedDateTime" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                                                   "RtWellKnownName" TEXT,
+                                                   "data" OBJECT(DYNAMIC),
+                                                   PRIMARY KEY ("Timestamp", "RtId", "CkTypeId")
+                                                 ) CLUSTERED INTO 3 SHARDS;
                                                  """;
     
     public const string DeleteTableIfExists = "drop table if exists {0};";
 
     public const string InsertStreamDataEntry =
         """
-        insert into {0} ("RtId", "CkTypeId", "Timestamp", data)
-        values (@RtId, @CkTypeId, @Timestamp, @data)
+        INSERT INTO {0} ("RtId", "CkTypeId", "Timestamp", "RtWellKnownName", data)
+        VALUES (@RtId, @CkTypeId, @Timestamp, @WellKnownName, @data)
         ON CONFLICT ("Timestamp", "RtId", "CkTypeId")
-        DO UPDATE SET "data" = "data" || EXCLUDED."data";
+        DO UPDATE SET
+            "data" = "data" || EXCLUDED."data",
+            "RtChangedDateTime" = CURRENT_TIMESTAMP,
+            "RtCreationDateTime" = "RtCreationDateTime"
         """;
 
     public const string InsertStreamDataBulk =
         """
-        INSERT INTO {0} ("RtId", "CkTypeId", "Timestamp", "data")
-        SELECT * FROM unnest(@rtIds, @ckTypeIds, @timestamps, @data)
+        INSERT INTO {0} ("RtId", "CkTypeId", "Timestamp", "RtWellKnownName" "data")
+        SELECT * FROM unnest(@rtIds, @ckTypeIds, @timestamps,@rtWellKnownNames, @data)
         ON CONFLICT ("Timestamp", "RtId", "CkTypeId")
-        DO UPDATE SET "data" = "data" || EXCLUDED."data";
+        DO UPDATE SET
+            "data" = "data" || EXCLUDED."data",
+            "RtChangedDateTime" = CURRENT_TIMESTAMP,
+            "RtCreationDateTime" = "RtCreationDateTime"
         """;
 }
