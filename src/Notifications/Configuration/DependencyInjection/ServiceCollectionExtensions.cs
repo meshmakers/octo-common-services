eb6d89ccb635b08006edfb0f5ac2656fb5d8a5e1
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-03-23 19:36:12 +0100

    AB#1901: New: Fix building nuget packages

--- src/Notifications/Configuration/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Notifications/Configuration/DependencyInjection/ServiceCollectionExtensions.cs
@@ -3,8 +3,16 @@ using Meshmakers.Octo.Services.Notifications.Services;
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
+/// <summary>
+/// Extensions for dependency injection's service collection
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds Notification components to the service collection
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
     public static IServiceCollection AddOctoNotification(
         this IServiceCollection services)
     {
