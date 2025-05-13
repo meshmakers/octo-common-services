216d185e6e9f65f93d5e887f8f1a8af9942a1194
Reimar Klammer <reimar.klammer@meshmakers.io>
2025-05-13 14:17:25 +0200

    Add more validation for migrations.

--- src/Infrastructure/Migrations/MigrationChecker.cs
+++ src/Infrastructure/Migrations/MigrationChecker.cs
@@ -10,27 +10,62 @@ public static class MigrationChecker
 
         if (typeContexts.Count == 0)
         {
-            throw MigrationException.NoMigrationsFound();
+            throw MigrationException.NoMigrationsFound(assembly);
         }
 
         foreach (var contextByConfig in typeContexts.GroupBy(x => x.MigrationAttribute.ConfigName))
         {
-            if(contextByConfig.All(x => x.MigrationAttribute.FromVersion != 0))
+            if (contextByConfig.All(x => x.MigrationAttribute.FromVersion != 0))
             {
                 throw MigrationException.NoInitialMigrationFound(contextByConfig.Key);
             }
+
+            var migrations = contextByConfig.OrderBy(x => x.MigrationAttribute.FromVersion).ToList();
+
+            var exceptions = new List<MigrationException>();
+
+            if (migrations.Any(x => x.MigrationAttribute.ToVersion == x.MigrationAttribute.FromVersion))
+            {
+                exceptions.Add(migrations.Where(x => x.MigrationAttribute.FromVersion == x.MigrationAttribute.ToVersion)
+                    .Select(x =>
+                        MigrationException.FromAndToVersionCantBeEqual(contextByConfig.Key,
+                            x.MigrationAttribute.FromVersion, x.MigrationAttribute.ToVersion)).First());
+            }
+
+            if (migrations.Any(x => x.MigrationAttribute.ToVersion < x.MigrationAttribute.FromVersion))
+            {
+                exceptions.Add(migrations.Where(x => x.MigrationAttribute.ToVersion < x.MigrationAttribute.FromVersion)
+                    .Select(x =>
+                        MigrationException.ToVersionLowerThanFromVersion(contextByConfig.Key,
+                            x.MigrationAttribute.FromVersion, x.MigrationAttribute.ToVersion)).First());
+            }
             
-            var migrations = contextByConfig.OrderBy(x=> x.MigrationAttribute.FromVersion).ToList();
+            if(migrations.Any(x => x.MigrationAttribute.ToVersion < 0 || x.MigrationAttribute.FromVersion < 0))
+            {
+                exceptions.Add(migrations.Where(x => x.MigrationAttribute.ToVersion < 0 || x.MigrationAttribute.FromVersion < 0)
+                    .Select(x =>
+                        MigrationException.NegativeVersion(contextByConfig.Key,
+                            x.MigrationAttribute.FromVersion, x.MigrationAttribute.ToVersion)).First());
+            }
+            
+            
+
             for (var i = 1; i < migrations.Count; i++)
             {
                 var current = migrations[i];
                 var previous = migrations[i - 1];
                 if (current.MigrationAttribute.FromVersion != previous.MigrationAttribute.ToVersion)
                 {
-                    throw MigrationException.GapInMigrationFound(contextByConfig.Key, previous.MigrationAttribute.ToVersion, previous.MigrationAttribute.FromVersion);
+                    exceptions.Add(MigrationException.GapInMigrationFound(contextByConfig.Key,
+                        previous.MigrationAttribute.ToVersion, previous.MigrationAttribute.FromVersion));
                 }
             }
-            
+
+            if (exceptions.Count > 0)
+            {
+                var message = string.Join(Environment.NewLine, exceptions.Select(x => x.Message));
+                throw MigrationException.MultipleMigrationsHaveErrors(message);
+            }
         }
     }
 }
--- src/Infrastructure/Migrations/MigrationException.cs
+++ src/Infrastructure/Migrations/MigrationException.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+﻿using System.Reflection;
 
 namespace Meshmakers.Octo.Services.Infrastructure.Migrations;
 
@@ -8,12 +8,28 @@ public class MigrationException : Exception
     {
     }
 
-    public static MigrationException NoMigrationsFound() => new("No migrations found.");
+    public static MigrationException NoMigrationsFound(Assembly assembly) =>
+        new($"No migrations found in assembly '{assembly.FullName}'");
 
     public static MigrationException NoInitialMigrationFound(string configName) =>
         new($"No initial migration (FromVersion=0) found for config {configName}.");
 
-    public static MigrationException GapInMigrationFound(string configName, int previousToVersion, int currentFromVersion)
-    => new("Gap in migration found. Previous migration ToVersion: " + previousToVersion +
-           ", current migration FromVersion: " + currentFromVersion + ". Config: " + configName);
+    public static MigrationException GapInMigrationFound(string configName, int previousToVersion,
+        int currentFromVersion)
+        => new("Gap in migration found. Previous migration ToVersion: " + previousToVersion +
+               ", current migration FromVersion: " + currentFromVersion + ". Config: " + configName);
+
+    public static MigrationException FromAndToVersionCantBeEqual(string configName, int fromVersion, int toVersion)
+        => new("From and To version can't be equal. Config: " + configName +
+               ", FromVersion: " + fromVersion + ", ToVersion: " + toVersion);
+
+    public static MigrationException ToVersionLowerThanFromVersion(string configName, int fromVersion, int toVersion)
+        => new("To version can't be lower than From version. Config: " + configName +
+               ", FromVersion: " + fromVersion + ", ToVersion: " + toVersion);
+
+    public static MigrationException NegativeVersion(string configName, int fromVersion, int toVersion)
+        => new("Version can't be negative. Config: " + configName +
+               ", FromVersion: " + fromVersion + ", ToVersion: " + toVersion);
+
+    public static MigrationException MultipleMigrationsHaveErrors(string message) => new(message);
 }
