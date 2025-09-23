1d61a31628dc8fddd775e9b4f2dc992dcfde0960
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-09-23 16:53:44 +0200

    AB#2655: New: Support of version ranges for dependent ck model libraries. Use of variables for dependent models

--- samples/SampleWebService/SampleServiceConstants.cs
+++ samples/SampleWebService/SampleServiceConstants.cs
@@ -2,7 +2,7 @@ namespace SampleWebService;
 
 internal static class SampleServiceConstants
 {
-    public const string ApiPathPrefix = "system/v{version:apiVersion}";
+    public const string ApiPathPrefix = "${System}/v{version:apiVersion}";
     public const string ApiVersion1 = "1.0";
     public const string ApiVersion2 = "2.0";
 }
